19cce55e51739f2d73e269db49e080336bd166c4
Serge <serge@orangebeard.io>
2021-12-09 14:40:45 +0100

    [team-soju#3848] Use latest release version of Java client. Remove superfluous unit test.

--- src/test/java/io/orangebeard/listener/OrangebeardListenerTest.java
+++ src/test/java/io/orangebeard/listener/OrangebeardListenerTest.java
@@ -6,7 +6,6 @@ import io.orangebeard.client.entity.StartTestItem;
 import io.orangebeard.client.entity.StartTestRun;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.UUID;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -82,88 +81,4 @@ public class OrangebeardListenerTest {
 
         verify(orangebeardClient, times(1)).startTestItem(any(), any());
     }
-
-    @Test
-    public void multiple_suites_can_be_tested() {
-        // Prepare test run and two test suites. Each suite contains two tests.
-        Description runDescription = Description.createSuiteDescription("Run Description");
-        UUID testRunUUID = UUID.fromString("49e7186d-e14d-4eeb-bc29-e36279d3b628");
-        when(orangebeardClient.startTestRun(any())).thenReturn(testRunUUID);
-
-        String suiteName1 = "com.example.suite1";
-        Description suiteDescription1 = Description.createSuiteDescription(suiteName1);
-        UUID suiteUuid1  = UUID.fromString("621d0026-0e28-47e0-921b-de5c8e9bd26f");
-
-        String testName11 = "test11";
-        Description testDescription11 = Description.createTestDescription(suiteName1, testName11);
-        UUID testUuid11  = UUID.fromString("c7173102-5456-11ec-bf63-0242ac130002");
-
-        String testName12 = "test12";
-        Description testDescription12 = Description.createTestDescription(suiteName1, testName12);
-        UUID testUuid12  = UUID.fromString("25af7061-cd31-4782-8513-80e6fdd30ad3");
-
-        String suiteName2 = "com.example.suite2";
-        Description suiteDescription2 = Description.createSuiteDescription(suiteName2);
-        UUID suiteUuid2  = UUID.fromString("e952fd8b-9624-444f-9bbc-cccca2e32d19");
-
-        String testName21 = "SubSuite 2.1";
-        Description testDescription21 = Description.createTestDescription(suiteName2, testName21);
-        UUID testUuid21  = UUID.fromString("c4be0af9-8f0f-478a-a66b-5697be40407a");
-
-        String testName22 = "SubSuite 2.2";
-        Description testDescription22 = Description.createTestDescription(suiteName2, testName22);
-        UUID testUuid22  = UUID.fromString("b3650fc1-41bc-4b00-beec-8c4c1467cfd0");
-
-        suiteDescription1.addChild(testDescription11);
-        suiteDescription1.addChild(testDescription12);
-
-        suiteDescription2.addChild(testDescription21);
-        suiteDescription2.addChild(testDescription22);
-
-        // Prepare the UUID's that orangebeardClient.startTestItem(...) should return.
-        when(orangebeardClient.startTestItem(any(), any(StartTestItem.class)))
-                .thenReturn(suiteUuid1)
-                .thenReturn(testUuid11)
-                .thenReturn(testUuid12)
-                .thenReturn(suiteUuid2)
-                .thenReturn(testUuid21)
-                .thenReturn(testUuid22);
-
-        // Prepare to capture the parameters passed to orangebeardClient.startTestItem(...).
-        ArgumentCaptor<StartTestItem> testItemCaptor = ArgumentCaptor.forClass(StartTestItem.class);
-        ArgumentCaptor<UUID> suiteUuidCaptor = ArgumentCaptor.forClass(UUID.class);
-
-
-        // Start the run, the suites, and the tests contained in the suites.
-        orangebeardListener.testRunStarted(runDescription);
-        orangebeardListener.testSuiteStarted(suiteDescription1);
-        orangebeardListener.testStarted(testDescription11);
-        orangebeardListener.testStarted(testDescription12);
-        orangebeardListener.testSuiteStarted(suiteDescription2);
-        orangebeardListener.testStarted(testDescription21);
-        orangebeardListener.testStarted(testDescription22);
-
-        // Verify the calls, and capture the parameters that were passed.
-        verify(orangebeardClient, times(6)).startTestItem(suiteUuidCaptor.capture(), testItemCaptor.capture());
-
-        // Verify that the tests were put in the proper suites.
-        List<StartTestItem> allStartTestItems = testItemCaptor.getAllValues();
-        List<UUID> allUuids = suiteUuidCaptor.getAllValues();
-        assertThat(allUuids.get(0)).isNull();
-        assertThat(allUuids.get(1)).isEqualTo(suiteUuid1);
-        assertThat(allUuids.get(2)).isEqualTo(suiteUuid1);
-        assertThat(allUuids.get(3)).isNull();
-        assertThat(allUuids.get(4)).isEqualTo(suiteUuid2);
-        assertThat(allUuids.get(5)).isEqualTo(suiteUuid2);
-
-        // Verify that the tests were registered with the right names.
-        assertThat(allStartTestItems.get(0).getName()).isEqualTo(suiteName1);
-        assertThat(allStartTestItems.get(1).getName()).isEqualTo(testName11);
-        assertThat(allStartTestItems.get(2).getName()).isEqualTo(testName12);
-
-        assertThat(allStartTestItems.get(3).getName()).isEqualTo(suiteName2);
-        assertThat(allStartTestItems.get(4).getName()).isEqualTo(testName21);
-        assertThat(allStartTestItems.get(5).getName()).isEqualTo(testName22);
-    }
-
 }
